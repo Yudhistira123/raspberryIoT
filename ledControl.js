64e3b4a864a01bdc4f1b61f353db6da315f3c991
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-10-07 15:52:53 +0700

    first commit

---
+++ ledControl.js
@@ -0,0 +1,20 @@
+import { Gpio } from "onoff";
+
+// Define GPIO pin 17 as output
+const led = new Gpio(17, "out");
+
+// Blink LED every second
+let value = 0;
+setInterval(() => {
+  value = value ^ 1; // toggle between 0 and 1
+  led.writeSync(value);
+  console.log(`LED is now ${value ? "ON" : "OFF"}`);
+}, 1000);
+
+// Cleanup on exit
+process.on("SIGINT", () => {
+  led.writeSync(0);
+  led.unexport();
+  console.log("Bye 👋");
+  process.exit();
+});
